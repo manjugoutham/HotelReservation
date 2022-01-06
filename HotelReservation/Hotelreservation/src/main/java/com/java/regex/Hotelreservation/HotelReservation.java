28fdb289ccb5d74279d84ca6cd27e303dae99dd2
manjugoutham <gouthamy219@gmail.com>
2022-01-06 14:15:39 +0530

    Hotel Reservation

---
+++ HotelReservation/Hotelreservation/src/main/java/com/java/regex/Hotelreservation/HotelReservation.java
@@ -0,0 +1,10 @@
+package com.java.regex.Hotelreservation;
+
+public class HotelReservation {
+	
+	public static void main(String args[])
+	{
+		System.out.println("Welcome to HotelReservation program");
+	}
+
+}
